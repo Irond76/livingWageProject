4c746c6fb914b91ab8da5149dc1d9aeeebe09003
Dale Bendixen <dalebendixen@gmail.com>
2024-02-06 13:29:20 -0500

    added notes

--- src/routes/Alabama/+page.svelte
+++ src/routes/Alabama/+page.svelte
@@ -8,14 +8,21 @@ import { DataTwoAdultsOneChild } from '../../data/AlabamaData';
 import { DataTwoAdultsTwoChildren } from '../../data/AlabamaData';
 import { DataTwoAdultsThreeChildren } from '../../data/AlabamaData';
 
+// Single Adult Data ===============================================
 const singleAdult = DataSingleAdult;
 const adultOneChild = DataSingleAdultOneChild;
 const adultTwoChildren = DataSingleAdultTwoChildren;
 const adultThreeChildren = DataSingleAdultThreeChildren;
+// =================================================================
+
+// Two Adults Data =================================================
 const twoAdults = DataTwoAdults;
 const twoAdultsOneChild = DataTwoAdultsOneChild;
 const twoAdultsTwoChildren = DataTwoAdultsTwoChildren;
 const twoAdultsThreeChildren = DataTwoAdultsThreeChildren
+// =================================================================
+
+
 // console.log(twoAdults);
 // console.log(twoAdultsOneChild);
 // console.log(twoAdultsTwoChildren);
