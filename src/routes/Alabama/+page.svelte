602ee12ecf77dcfcda2bbcdf8b86da307a726ab7
Dale Bendixen <dalebendixen@gmail.com>
2024-02-06 17:35:24 -0500

    added first chart and styled

--- src/routes/Alabama/+page.svelte
+++ src/routes/Alabama/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import { onMount } from 'svelte';
 import  { DataSingleAdult } from '../../data/AlabamaData';
 import { DataSingleAdultOneChild } from '../../data/AlabamaData';
 import { DataSingleAdultTwoChildren } from '../../data/AlabamaData';
@@ -29,6 +30,59 @@ const industrieSalaries = DataIndustryWages;
 // =================================================================
 
 
+function createchart1() {
+    const ctx = document.getElementById('myChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ["State Minimum Wage", "State Living Wage"],
+      datasets: [{
+        data: [`${singleAdult.adult.stateMinimumWage}`, `${singleAdult.adult.stateLivingWage}`],
+        borderWidth: 1,
+        backgroundColor: ['#a98467', '#FFE8D6'], 
+        barPercentage: .75
+      }],
+    },
+    options: {
+        indexAxis: 'y',
+      scales: {
+        x: {
+            ticks: {
+                color: '#fff'
+            },
+        grid: {
+            drawTicks: false,
+            color: '#525252'
+        },
+        },
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: '#fff'
+          },
+          grid: {
+            drawTicks: false,
+            color: '#525252'
+        }
+        }
+      },
+      plugins: {
+        title: {
+                display: true,
+                text: 'Minimum Wage Vs. Living Wage',
+                color: '#fff'
+            },
+        legend: {
+                display: false,
+        },
+      }
+    }
+  });
+}
+onMount(
+    createchart1
+)
+
 </script>
 <header>
     <div class="state-header-container">
@@ -36,12 +90,18 @@ const industrieSalaries = DataIndustryWages;
         <img src={img} alt="alabama">
     </div>
 </header>
+<main>
+    <div class="chart-one">
+        <canvas id="myChart" ></canvas>
+      </div>
+</main>
+
 
 
 <style>
      header {
         background-color: #6B705C;
-        height: fit-content;
+        height: 40dvh;
         color: #e3e3e3;
         padding: 1em 0;
     }
@@ -49,5 +109,19 @@ const industrieSalaries = DataIndustryWages;
         text-align: center;
     }
 
+    .chart-one {
+        display: grid;
+        width: 30dvw;
+        height: 30dvh;
+        background-color: #424242;
+        margin-top: 1em;
+    }
+    main {
+        display: grid;
+        width: 100dvw;
+        height: 60dvh;
+        background-color: #B7B7A4;
+        justify-items: center;
+    }
 
 </style>
